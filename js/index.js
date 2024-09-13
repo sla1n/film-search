531d3711d7578bb0974a477bb3dca18d8cc3b55a
l3vick <vitalikige@gmail.com>
2024-09-13 16:20:09 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,95 @@
+const APIkey = 'b74c9412'
+let searchIdArray = []
+const searchForm = document.getElementById('search-form')
+const mainEl = document.getElementById('main-section')
+
+mainEl.innerHTML = `<img src="img/no-data-initial.png" alt="">`
+
+searchForm.addEventListener('submit', async (e)=>{
+	e.preventDefault()
+	
+	const searchInput = document.getElementById('search-input').value 
+	const res = await fetch(`http://www.omdbapi.com/?s=${searchInput}&type=movie&apikey=${APIkey}`)
+	const data = await res.json()
+	searchIdArray = []
+	
+	if (data.Response === "True"){
+		for (film of data.Search) searchIdArray.push(film.imdbID)
+		mainEl.classList.remove('error')
+		mainEl.innerHTML = await getHtmlMovies(searchIdArray)
+	}
+	else {
+		mainEl.classList.add('error')
+		mainEl.innerHTML = getErrorHtml()
+	}
+	})
+
+
+
+document.addEventListener('click', (e) => {
+	if(e.target.dataset.movie){
+		const targetEl = e.target
+		handleAddClick(targetEl.dataset.movie)
+		targetEl.classList.remove("fa-plus", "add-icon")
+		targetEl.classList.add("fa-check", "added-icon")
+	}
+})
+
+function getErrorHtml(){
+	return `
+	
+	<p>Unable to find what you’re looking for. Please try another search.</p>
+
+	`
+}
+
+async function getHtmlMovies(arr) {
+	let moviesHtml = ''
+	for (id of arr){
+		const res = await fetch(`http://www.omdbapi.com/?i=${id}&apikey=${APIkey}`)
+		const dataMovie = await res.json()
+		let classesOfIcon = ""
+	
+		if(localStorage.getItem(id)){
+			classesOfIcon = "fa-check added-icon"
+		}
+		else{
+			classesOfIcon = "fa-plus add-icon"
+		}
+		
+				moviesHtml += `
+				<div class="movie-container">
+					<img src="${dataMovie.Poster}" alt="">
+					<div>
+						<div class="name-container">
+							<p class="movie-name">${dataMovie.Title}</p>
+							<i class="fa-solid fa-star star-icon"></i>
+							<p class="movie-rating">${dataMovie.imdbRating}</p>
+						</div>
+						<div class="features-container">
+							<p>${dataMovie.Runtime}</p>
+							<p>${dataMovie.Genre}</p>
+							<div class="add-watchlist-container">
+								<i class="fa-solid ${classesOfIcon}" data-movie = '${dataMovie.imdbID}'></i>
+								<p>Watchlist</p>
+							</div>
+						</div>
+						<p class="description-text">${dataMovie.Plot}</p>
+					</div>
+				</div>
+				
+				`
+				
+			}
+	return moviesHtml
+}
+	
+	
+
+
+async function handleAddClick(movieId){
+	const res = await fetch(`http://www.omdbapi.com/?i=${movieId}&apikey=${APIkey}`)
+	const data = await res.json()
+	localStorage.setItem(data.imdbID, data)
+}
+
---
+++ js/watchlist.js
@@ -0,0 +1,65 @@
+const APIkey = 'b74c9412'
+
+
+document.addEventListener('click', (e) => {
+	if(e.target.dataset.movie){
+		handleRemoveClick(e.target.dataset.movie)
+	}
+})
+
+
+function handleRemoveClick(id){
+	localStorage.removeItem(id)
+	renderWatchlist()
+}
+
+async function renderWatchlist(){
+	document.getElementById('main-section').innerHTML = await getWatclistHtml()
+}
+
+
+async function getWatclistHtml(){
+	let watchlistHtml = ''
+	for (var i = 0; i < localStorage.length; i++){
+		const res = await fetch(`http://www.omdbapi.com/?i=${localStorage.key(i)}&apikey=${APIkey}`)
+		const dataMovie = await res.json()
+		watchlistHtml += 
+		`
+		<div class="movie-container">
+					<img src="${dataMovie.Poster}" alt="">
+					<div>
+						<div class="name-container">
+							<p class="movie-name">${dataMovie.Title}</p>
+							<i class="fa-solid fa-star star-icon"></i>
+							<p class="movie-rating">${dataMovie.imdbRating}</p>
+						</div>
+						<div class="features-container">
+							<p>${dataMovie.Runtime}</p>
+							<p>${dataMovie.Genre}</p>
+							<div class="add-watchlist-container">
+								<i class="fa-solid fa-minus remove-icon" data-movie = '${dataMovie.imdbID}'></i>
+								<p>Remove</p>
+							</div>
+						</div>
+						<p class="description-text">${dataMovie.Plot}</p>
+					</div>
+				</div>
+				
+				
+		`
+	}
+	if(localStorage.length > 0) return watchlistHtml
+	else {
+		document.getElementById('main-section').classList.add('no-movies')
+		return `<p class="main-text">Your watch list is looking a little empty...</p>
+			<div class="add-container">
+				<i class="fa-plus add-icon"></i>
+				<p class="add-text">Let's add some movies</p>
+			</div>`}
+
+}
+
+
+
+renderWatchlist()
+// localStorage.clear()
